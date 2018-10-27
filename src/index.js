077d53a0fbe5ace5f8dd801f9ab9bd9b7c58aad6
lukewiwa <luke.wiwa@gmail.com>
2018-10-28 00:44:07 +1100

    remove redundant service worker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { registerServiceWorker } from './register-service-worker'
 import './scss/app.scss'
 
 // main app
 import App from './App'
-
-registerServiceWorker('./service-worker.js')
 
 ReactDOM.render((
   <div className="wrapper">
